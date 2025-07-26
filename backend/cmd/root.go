dd59ca6970ff180d0ca5364824217166f7ed9f3b
Oleg Lobanov <oleg@lobanov.me>
2025-07-26 13:02:48 +0400

    ok

--- backend/cmd/root.go
+++ backend/cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"go.uber.org/automaxprocs/maxprocs"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -19,6 +20,7 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	_, _ = maxprocs.Set()
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
